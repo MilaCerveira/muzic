b61116e4b489bb760b819c72395da882b4cfeeeb
user <user@users-MBP.broadband>
2021-12-01 10:21:25 +0000

    first commit

---
+++ src/containers/MusicChartContainer.js
@@ -0,0 +1,10 @@
+import {useState, useEffect} from "react";
+const MusicChartContainer = () => {
+    return(
+        <>
+        <h1>what up dawg</h1>
+        </>
+    )
+}
+
+export default MusicChartContainer;
